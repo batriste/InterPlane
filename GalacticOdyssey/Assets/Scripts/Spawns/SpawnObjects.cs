0289e4f20a661cbff0a136b2e3f509919780210f
batriste <48353924+batriste@users.noreply.github.com>
2023-02-12 22:02:59 +0100

    ArregladoJuego
    El juego es "Jugable" Faltarian diversos spawns o objetos, que los collider salgan donde toca ya que los planetas no he podido hacer mas y añadir unos creditos finales. ademas de acabar el juego que esta en pañales, esta fue una gran experiencia de 24h y por ello solo queria que el juego ahora mismo solo fuera jugable. Dicho esto dejo mi codigo libre y abierto a que quien quiera contacte conmigo zoarialpainter@gmail.com
    y lo pueda usar en cualquier proyecto.
    Respecto a la historia y assets son de mis compañeros y no creo que quieran que sean libres. Disfrutad y gracias

--- GalacticOdyssey/Assets/Scripts/Spawns/SpawnObjects.cs
+++ GalacticOdyssey/Assets/Scripts/Spawns/SpawnObjects.cs
@@ -9,7 +9,7 @@ public class SpawnObjects : MonoBehaviour
     public float spawnInterval = 5f, randomPosY;
     private float timeSinceLastSpawn;
     public GameObject[] objectsToSpawn;
-    public int selectedObjectToSpawn = 1;
+    public int selectedObjectToSpawn = 6;
     public int MaxSpawn;
     // Start is called before the first frame update
     public int LastInvoque;
@@ -24,25 +24,22 @@ public class SpawnObjects : MonoBehaviour
         if (timeSinceLastSpawn >= spawnInterval)
         {
             randomPosY = Random.Range(minY, maxY);
-            transform.position = new Vector3(transform.position.x, randomPosY, transform.position.z);
+            
             
             timeSinceLastSpawn = 0f;
-            if (selectedObjectToSpawn > MaxSpawn)
+            while(selectedObjectToSpawn == LastInvoque)
             {
-                selectedObjectToSpawn = Random.Range(0, MaxSpawn-1);
-                if (LastInvoque == selectedObjectToSpawn)
-                {
-                    selectedObjectToSpawn = Random.Range(0, MaxSpawn-1);
-                    do
-                    {
-                        selectedObjectToSpawn = Random.Range(0, MaxSpawn);
-                    } while (LastInvoque != selectedObjectToSpawn);
-                }
+                selectedObjectToSpawn = Random.Range(0, MaxSpawn);
+                Debug.Log(selectedObjectToSpawn);
             }
             
-            LastInvoque = selectedObjectToSpawn;
-            Instantiate(objectsToSpawn[selectedObjectToSpawn]);
             
+           
+            LastInvoque = selectedObjectToSpawn;
+            GameObject spawned  =Instantiate(objectsToSpawn[selectedObjectToSpawn]);
+            //Cada objeto deberemos asignar bien su transform de X e Z para que spawneen fuera del mapa
+            spawned.transform.position = new Vector3(spawned.transform.position.x, randomPosY, spawned.transform.position.z);
+
         }
     }
     //Duda aqui no se que hare porque necesito alguien que me detecte colisiones
